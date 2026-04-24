d8dbf796a58d1cd49c048b6b616eb407c24db448
mtbsteve <schiwo1@gmail.com>
2026-04-24 15:44:44 +0200

    Initial Commit

---
+++ TibberWatch Watch App/TibberWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TibberWatchApp.swift
+//  TibberWatch Watch App
+//
+//  Created by STEPHAN SCHINDEWOLF on 24.04.26.
+//
+
+import SwiftUI
+
+@main
+struct TibberWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
